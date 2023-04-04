33bec227c5a4b8c922cfcf18ee3c6aa46ecb19ca
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-04-04 19:06:36 -0300

    Feat: add SimpleReport class

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,35 @@
+from datetime import datetime
+from collections import Counter
+
+
+current_date = datetime.now()
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(lista: list[dict]) -> str:
+        max_fab_date = max(
+            lista,
+            key=lambda sub: abs(
+                datetime.strptime(sub["data_de_fabricacao"], "%Y-%m-%d")
+                - current_date
+            ),
+        )["data_de_fabricacao"]
+
+        min_valid_date = min(
+            lista,
+            key=lambda sub: abs(
+                datetime.strptime(sub["data_de_validade"], "%Y-%m-%d")
+                - current_date
+            ),
+        )["data_de_validade"]
+
+        companies = Counter([product["nome_da_empresa"] for product in lista])
+        most_repet_companie = companies.most_common()[0][0]
+
+        result = (
+            f"Data de fabricação mais antiga: {max_fab_date}\n"
+            f"Data de validade mais próxima: {min_valid_date}\n"
+            f"Empresa com mais produtos: {most_repet_companie}"
+            )
+        return result
