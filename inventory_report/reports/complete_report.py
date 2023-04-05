bcf4a04118eb1af74c8356fec3a01360052d9f9c
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-04-04 21:09:03 -0300

    Feat: add CompleteReport class

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(lista: list[dict]) -> str:
+        simple_report = SimpleReport.generate(lista)
+        companies = Counter([product["nome_da_empresa"] for product in lista])
+
+        stocks = ""
+        for company, amount in companies.items():
+            stocks += f"- {company}: {amount}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{stocks}"
+            )
