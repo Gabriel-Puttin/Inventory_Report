13a86e7302dd27203141cd8483618be0a2f8ec6a
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-04-06 19:04:22 -0300

    Feat: build func main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,32 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
 def main():
-    pass
+    try:
+        inventory = {}
+        _, file, type_report = sys.argv
+        path = sys.argv[1].split(".")[-1]
+
+        if path == "csv":
+            inventory = InventoryRefactor(CsvImporter)
+        elif path == "json":
+            inventory = InventoryRefactor(JsonImporter)
+        else:
+            inventory = InventoryRefactor(XmlImporter)
+
+        inventory.import_data(file, type_report)
+        reports = {
+            "simples": SimpleReport.generate(inventory.data),
+            "completo": CompleteReport.generate(inventory.data),
+        }
+
+        return sys.stdout.write(reports[type_report])
+
+    except ValueError:
+        sys.stderr.write("Verifique os argumentos\n")
